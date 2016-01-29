1d10d3725a434f51f19cca74ae5bf365745fee80
Ron Kurr <kurr@kurron.org>
2016-01-29 19:34:15 +0000

    Added Docker registry writer group

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,17 @@ resource "aws_iam_policy_attachment" "docker-registry-administrator-policy-attac
     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
 
+resource "aws_iam_group" "docker-registry-writer" {
+    name = "docker-registry-writer"
+    path = "/"
+}
+
+resource "aws_iam_policy_attachment" "docker-registry-writer-policy-attachment" {
+    name = "docker-registry-writer-policy-attachment"
+    groups = ["${aws_iam_group.docker-registry-writer.id}"]
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
 # ------------ outputs ----------------------
 
 output "bob" {
