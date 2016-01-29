f9a216f0e8f137b16101b45ccca06a4431389939
Ron Kurr <kurr@kurron.org>
2016-01-29 19:56:48 +0000

    Removed the repository because we will create them using the AWS CLI

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,12 +39,13 @@ resource "aws_iam_policy_attachment" "docker-registry-reader-policy-attachment" 
     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_ecr_repository" "test" {
-  name = "test"
-}
+#resource "aws_ecr_repository" "test" {
+  #name = "test"
+#}
 
 # ------------ outputs ----------------------
 
 output "repository_arn" {
-    value = "${aws_ecr_repository.test.arn}"
+#   value = "${aws_ecr_repository.test.arn}"
+    value = "something fake"
 }
