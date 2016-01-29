af58f866696d8d0c7ba1266f02da684fa8f1cd70
Ron Kurr <kurr@kurron.org>
2016-01-29 21:44:55 +0000

    Migration script seems to work

--- bash/migrate-images.sh
+++ bash/migrate-images.sh
@@ -4,21 +4,28 @@
 
 set +x
 
-REPOSITORY=3871-8830-8760.dkr.ecr.us-east-1.amazonaws.com
+#REPOSITORY=3871-8830-8760.dkr.ecr.us-east-1.amazonaws.com
+REPOSITORY=711226717742.dkr.ecr.us-east-1.amazonaws.com
+
+# authenticate our Docker client with Amazon
+$(aws ecr get-login --region us-east-1)
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     PULL_COMMAND="docker pull $line"
-    echo "$PULL_COMMAND"
+    $PULL_COMMAND
     oldTag=$(cut -d ':' -f 2 <<< $line)
     noRegistry="${line#*/}"
     imageName="${noRegistry%:*}"
-    CREATE_REPOSITORY_COMMAND="aws ecr create-repository --repository-name $imageName --region $AWS_REGION"
-    echo "$CREATE_REPOSITORY_COMMAND"
+    CREATE_REPOSITORY_COMMAND="aws ecr create-repository --repository-name $imageName --region us-east-1"
+    echo $CREATE_REPOSITORY_COMMAND
+    $CREATE_REPOSITORY_COMMAND
     newTag="$REPOSITORY/$imageName:$oldTag"
     TAG_COMMAND="docker tag $line $newTag"
-    echo "$TAG_COMMAND"
+    echo $TAG_COMMAND
+    $TAG_COMMAND
     PUSH_COMMAND="docker push $newTag"
-    echo "$PUSH_COMMAND"
+    echo $PUSH_COMMAND
+    $PUSH_COMMAND
     echo
     echo
 done < "images-to-upload.txt"
